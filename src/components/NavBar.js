9805d6fce5ac86e968104c8dee55ff8a7b6e10ed
yongdae <7942yongdae@gmail.com>
2020-07-27 23:48:14 +0900

    Change a home url

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,7 +7,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 const useStyles = makeStyles((theme) => ({
   appBar: {
     color: '#000000e8',
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
   },
   offset: theme.mixins.toolbar,
   title: {
@@ -46,20 +45,15 @@ function NavBar() {
       <AppBar className={classes.appBar} elevation={0} position="fixed">
         <Toolbar className={classes.toolbar}>
           <div className={classes.left} />
-          <Link variant="h6" underline="none" color="inherit" className={classes.title} href="/">
+          <Link variant="h6" underline="none" color="inherit" className={classes.title} href="/white-noise">
             {'백 색 소 음'}
           </Link>
           <div className={classes.right} />
         </Toolbar>
-
       </AppBar>
       <div className={classes.offset} />
     </div>
   );
 }
-
-NavBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default NavBar;
